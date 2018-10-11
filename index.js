6beda25ead7bce960e974f68840c2e7a39036f8c
Ethan Murphy <verakai141@gmail.com>
2018-10-11 11:31:34 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,60 @@
-// Add your functions and code here
+var kittens= ['Milo', 'Otis', 'Garfield'];
+
+//Test 1: Destructively Append 'Ralph'
+
+function destructivelyAppendKitten(){
+  kittens.push('Ralph');
+return kittens;
+}
+
+
+//Test 2: Destructively Prepend 'Bob'
+
+function destructivelyPrependKitten(){
+   kittens.unshift('Bob');
+   return kittens;
+}
+
+
+//Test 3: Destructively remove last Kitten
+
+function destructivelyRemoveLastKitten(){
+  kittens.pop();
+return kittens;
+}
+
+
+//Test 4: Destructively remove first Kitten
+
+function destructivelyRemoveFirstKitten(){
+    kittens.shift([0]);
+return kittens;
+}
+
+
+//Test 5: Append Kitten 'Broom'
+
+function appendKitten(){
+  return [...kittens, 'Broom'];
+}
+  
+
+//Test 6: Prepend Kitten 'Arnold'
+
+function prependKitten(){
+  return ['Arnold',...kittens];
+}
+
+
+//Test 7: Remove the last Kitten
+
+function removeLastKitten(){
+  
+}
+
+
+//Test 8: Remove the first Kitten
+
+function removeFirstKitten(){
+  
+}
